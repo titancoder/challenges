d7856d6e5b4d970b2fbe62b34cc41e60a7778c1f
Mangesh Kumar <titancoder@hotmail.com>
2021-02-19 17:19:30 +0530

    Add fiveDiceScore

--- challenges.js
+++ challenges.js
@@ -36,4 +36,44 @@ module.exports = class Challenges {
     console.log(`isPangram`, val);
     return val;
   }
+
+  static fiveDiceScore(diceArray) {
+    const initialValues = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 };
+    diceArray.map(
+      (el) => (initialValues[`${el}`] = initialValues[`${el}`] + 1)
+    );
+    const values = Object.values(initialValues);
+    const keys = Object.keys(initialValues);
+    let scr = 0;
+    values.map((el, i) => {
+      if (el >= 3) {
+        if (el > 3) {
+          if (keys[i] == 1) {
+            scr += 1100;
+          } else if (keys[i] == 5) {
+            scr += 550;
+          } else {
+            scr += parseInt(`${keys[i]}00`);
+          }
+        } else {
+          if (keys[i] == 1) {
+            scr += 1000;
+          } else {
+            scr += parseInt(`${keys[i]}00`);
+          }
+        }
+      } else {
+        if (el == 1 || el == 2) {
+          keys[i] == 1
+            ? (scr += el * 100)
+            : keys[i] == 5
+            ? (scr += el * 50)
+            : (scr += 0);
+        }
+      }
+    });
+
+    console.log(`fiveDiceScore`, scr);
+    return scr;
+  }
 };
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const Challenges = require("./challenges");
 
 //Challenges.isPangram("The quick brown fox jumps over the lazy dog");
+//Challenges.fiveDiceScore([3, 3, 3, 3, 3]);
